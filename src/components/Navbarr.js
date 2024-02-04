80bdf88eaec8d267fb5c317345f2cc6b49900113
Dashrath-chaudhary <121454236+Dashrath-chaudhary@users.noreply.github.com>
2024-02-04 23:39:39 +0530

    sidebar changed

--- src/components/Navbarr.js
+++ src/components/Navbarr.js
@@ -20,40 +20,49 @@ const Navbarr = (props) => {
   
 
   return (
-    <div className='flex w-full z-50 h-[60px] text-white fixed justify-between items-center bg-gray-800'>
-      {<ul
+    <div className='flex w-full z-50 h-[60px] text-white fixed bg-gray-800'>
+      {<div
         ref={sidebarRef}
-        className={`fixed w-2/4  flex flex-col z-50   h-screen top-0 bg-gray-800 items-center justify-around  transition-all ease-in-out duration-300 ${
+        className={`fixed w-2/4  flex flex-col z-50 md:hidden h-screen top-0 bg-gray-800 gap-20 transition-all ease-in-out duration-300 ${
           nav ? "translate-x-0 overflow-y-hidden" : "translate-x-[-900px]"
         }`}
       >
-        <div className='flex items-center gap-2'>
-          <p className='ml-8 text-white text-2xl font-extrabold'>Portfolio</p>
+        <div className='flex w-full flex-col mt-10 items-center gap-5'>
+          <p className=' text-white text-2xl font-extrabold'>Portfolio</p>
+          <div className='h-[0.5px] w-full bg-slate-50'></div>
         </div>
         
-        {navItems.map((item) => (
-          <a
-            href={`#${item.title}`}
-            onClick={navHandler}
-            className='w-full h-10 flex cursor-pointer items-center hover:shadow-lg hover:border-t hover:border-b justify-center'
-            key={item.id}
-          >
-            <li>{item.title}</li>
-          </a>
-        ))}
-      </ul>}
+        <div>
+            {navItems.map((item) => (
+              <a
+                href={`#${item.title}`}
+                onClick={navHandler}
+                className='w-full h-10 flex cursor-pointer items-center capitalize hover:shadow-lg hover:border-t hover:border-b justify-center'
+                key={item.id}
+              ><ul>
+                <li>{item.title}</li>
+              </ul>
+                
+              </a>
+            ))}
+        </div>
+       
+      </div>}
 
       <div className='w-full flex items-center justify-between'>
       
         <div className='ml-8 font-bold text-2xl'>Portfolio</div>
 
-        <ul className='gap-4 mr-4 hidden bg-zinc- rounded-md md:flex'>
+        <div className='gap-4 mr-4 hidden bg-zinc- rounded-md md:flex'>
           {navItems.map((item) => (
-            <li className='text-xl hover:bg-white hover:text-black cursor-pointer py-1 px-2 rounded-md' key={item.id}>
-              <a href={`#${item.title}`}>{item.title}</a>
-            </li>
+            <ul>
+              <li className='text-xl hover:bg-white hover:text-black cursor-pointer py-1 px-2 rounded-md' key={item.id}>
+                <a href={`#${item.title}`}>{item.title}</a>
+              </li>
+            </ul>
+           
           ))}
-        </ul>
+        </div>
 
         <div className='flex md:hidden mt-2 absolute right-0 mr-8'>
           {!nav ? (
